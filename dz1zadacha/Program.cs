24373799b634bbc92f8aae7013953571c3eaaa96
Menua <menya2012avah@mail.ru>
2022-10-12 13:36:42 +0300

    1dz

---
+++ dz1zadacha/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+ 
+void PolindromsNum(int number)
+{
+    if(number >= 10000)
+    {
+        int division1 = number / 10000;
+        int remainder1 = number % 10;
+ 
+            if(division1 == remainder1)
+            {
+                number = number / 10;
+                int division2 = number / 100;
+                int remainder2 = number % 10;
+                
+                Console.WriteLine("Да");
+            
+            }
+            if(division1 != remainder1 ) 
+            Console.WriteLine("Нет");
+            
+    }
+    else
+    Console.WriteLine("Некорректное число!");
+}
+ 
+Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine()!);
+PolindromsNum(number);
